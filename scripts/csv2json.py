ddf1618bf43e60f52db57ff78c7027a556d9ab4a
John Rinehart <johnrichardrinehart@gmail.com>
2015-10-16 22:51:01 -0400

    plotter works. columns resize. needs to be cleaned up.

--- scripts/csv2json.py
+++ scripts/csv2json.py
@@ -1,11 +1,14 @@
 import csv, json, sys, re, argparse, datetime, time
 
-def csv2json(infilepath, delim, keyarray, dateformat):
+def csv2json(infilepath="../ArnaudWeights.csv", delim=",",
+        keyarray=["Lift","Weight","Reps","Sets","Date","Comment"],
+        dateformat="%d-%b-%Y"):
     """ Converts an input csv file to json treating each line as a
         document. Example usage: 
         python csv2json.py --infile="..\ArnaudWeights.csv" --delim=","
-        --fieldlist=Lift,Weight,Reps,Sets,Date,Comment """
-    print dateformat
+        --fieldlist=Lift,Weight,Reps,Sets,Date,Comment
+        --dateformat="%d-%b-%Y" """
+    #print dateformat
     with open(infilepath,'rb') as csvfile:
         spamreader = csv.reader(csvfile, delimiter = delim)
         # spamreader is a list of lines. Each line is a list of words.
@@ -16,21 +19,25 @@ def csv2json(infilepath, delim, keyarray, dateformat):
         for line in spamreader:
             linedict = {}
             itemcnt = 0
+            #print line
             for item in line:
-                if linecnt > 1: # skip first line
-                    print "line number: " + str(linecnt)
+                if linecnt > 0: # skip first line
+                    #print "line number: " + str(linecnt)
                     lineword = item.lstrip().rstrip()
-                    print "Field being processed: " + keyarray[itemcnt]
-                    print "Value being processed: " + lineword
+                    #print "Field being processed: " + keyarray[itemcnt]
+                    #print "Value being processed: " + lineword
                     if keyarray[itemcnt].lower() == ('DATE'.lower() or
                             'DATES'.lower()):
-                        print lineword
+                        #print lineword
                         date = datetime.datetime.strptime(lineword, dateformat)
                         lineword = date.isoformat()
                     linedict[keyarray[itemcnt]] = lineword
-                    itemcnt += 1
-                linecnt += 1
-            kvpairs.append(linedict)
+                else:
+                    linedict[keyarray[itemcnt]] = keyarray[itemcnt]
+                itemcnt += 1
+            linecnt += 1
+            if bool(linedict): #check if linedict is empty
+                kvpairs.append(linedict)
         with open('out.json', 'w') as outfile:
             json.dump(kvpairs, outfile)
 
@@ -44,7 +51,12 @@ args = parser.parse_args()
 # put the parsed arguments in args
 infile = args.infile
 delimiter = args.delim
-keys = args.fieldlist.split(',')
-dtfmt = args.dateformat
-# make the variables that we'll pass to csv2json
-csv2json(infile, delimiter, keys, dtfmt)
+print type(args)
+if args.infile is None and args.delim is None and args.fieldlist is None and args.dateformat is None:
+    print "No arguments specified. Assuming defaults."
+    csv2json()
+else:
+    keys = args.fieldlist.split(',')
+    dtfmt = args.dateformat
+    # make the variables that we'll pass to csv2json
+    csv2json(infile, delimiter, keys, dtfmt)
--- scripts/htdocs/src/js/app.js
+++ scripts/htdocs/src/js/app.js
@@ -1,44 +1,132 @@
-var app = angular.module('app',['ui.grid']);
+var app = angular.module('app',['ui.grid','ui.grid.resizeColumns']);
 
 app.controller('FitnessGrid', ['$scope', '$http', 
         function($scope, $http) {
-            var weights = []; var dates = [];
+            var weights = []; // TODO: Deprecate 
+            var dates = []; // TODO: Deprecate
+            var columns = [];
+            var plotData = {}; // Data filtered for plotting
+            var inData = {}; // All the data
+            $scope.gridOptions = {};
 
-            $scope.gridOptions = {
+            $scope.selection = {
+                ids: {"dl": false, 
+                    "pc": false,
+                    "sn": false,
+                    "os": false,
+                    "pp": false}
             };
+
+            $scope.lifts = [
+            {shortName: "dl",
+                properName: "Deadlift"}
+            ,
+            {shortName: "pc",
+                properName: "Power Clean"}
+            ,
+            {shortName: "sn",
+                properName: "Snatch"}
+            ,
+            {shortName: "os",
+                properName: "Overhead Squat"}
+            ,
+            {shortName: "pp",
+                properName: "Push Press"}
+            ];
             
             $scope.gridOptions.columnDefs = [
                 { name: 'Lift'}, 
-                { name : 'Date', type: 'date', cellFilter: 'date:"yyyy-MM-dd"'}, 
+                { name : 'Date', type: 'date', cellFilter: 'date:"dd-MMM-yy"'}, 
                 { name: 'Reps' }, 
                 { name: 'Sets' }, 
                 { name: 'Weight', type: 'number'},
                 { name: 'Comment'},
             ];
 
+            //$scope.gridOptions.enableColumnResizing = true;
+
             $http.get('out.json').success(function(data) {
-                $scope.gridOptions.data = data;
-                $scope.makeArray(data);
-                var chart = c3.generate({
-                    bindto: '#chart',
-                    data: {
-                        x: 'dates',
-                        x_format: '%d-%m-%Y',
-                        columns: [ weights, dates ] },
-                    axis: {
-                        x: {
-                            type: 'timeseries'
-                        }
-                    }
-                });
+                $scope.gridOptions.data = data; // Make grid 
+                inData = data; // Make arrays for C3.js
             });
 
-            $scope.makeArray = function (dataIn) {
-                weights.push('weights');
-                dates.push('dates');
-                for (var doc in dataIn) {
-                    weights.push(dataIn[doc]['Weight']);
-                    dates.push(dataIn[doc]['Date']);
+            $scope.updateChart = function() {
+                var newPlotData = dataArrayGen();
+                columnified =  columnifyPlotData(newPlotData);
+                columns = columnified[0];
+                xsys = columnified[1];
+                //console.log(columns);
+                //console.log(xsys);
+                var chartData = {
+                    xs: xsys,
+                    xFormat: '%Y-%m-%dT%H:%M:%S',
+                    columns: columns,
                 };
+
+                var chartAxis = {
+                    x: { type: 'timeseries' },
+                };
+                
+                var chart = c3.generate({
+                    bindto: '#chart', 
+                    data: chartData,
+                    axis: chartAxis,
+//                    size: {
+//                        height: 300,
+//                        width: 700
+//                    }
+                });
+
             };
+
+            var columnifyPlotData = function(inPlotData){
+                var columnified = [];
+                var weightssubarray = [];
+                var datessubarray = [];
+                var temparray = [];
+                var includedlifts = [];
+                inPlotData.forEach( function(el,idx,inData) {
+                    liftsubarrayidx = includedlifts.indexOf(el.Lift);
+                    if (liftsubarrayidx !== -1){
+                        // Then we've already gotten to it
+                        weightssubarray[liftsubarrayidx].push(el.Weight);
+                        datessubarray[liftsubarrayidx].push(el.Date);
+                    }
+                    else {
+                        includedlifts.push(el.Lift);
+                        liftsubarrayidx = includedlifts.indexOf(el.Lift);
+                        // update the includedlifts to include this lift
+                        weightssubarray[liftsubarrayidx] = [];
+                        weightssubarray[liftsubarrayidx][0] = el.Lift;
+                        weightssubarray[liftsubarrayidx].push(el.Weight);
+                        // add all the weights
+                        datessubarray[liftsubarrayidx] = [];
+                        datessubarray[liftsubarrayidx][0] = el.Lift + ' Dates';
+                        datessubarray[liftsubarrayidx].push(el.Date);
+                        // add all the dates
+                    };
+                });
+                columnified = weightssubarray.concat(datessubarray);
+                var xsysobj = {}; // stores information regarding x's and y's
+                includedlifts.forEach(function(el,idx,weightsarray){
+                    xsysobj[el] = el + ' Dates';
+                });
+                //console.log(xsysobj);
+                return [columnified,xsysobj];
+            };
+
+            var dataArrayGen = function(){
+                plotData = inData.filter(function(dataPt){
+                    var lifts = $scope.lifts;
+                    for (lift of lifts){
+                        if ( (dataPt.Lift === lift.properName) && 
+                                $scope.selection.ids[lift.shortName] ){
+                        return true;
+                        };
+                    };
+                });
+                return plotData;
+            };
+            
+
         }]);
